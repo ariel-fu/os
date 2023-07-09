0055f2f44203f6a210bb4b34e5b06de410d35227
arielfu <arielfu@uw.edu>
2023-07-09 14:18:17 -0700

    add lock and unlock

--- kernel/file.c
+++ kernel/file.c
@@ -4,199 +4,208 @@
 
 #include <cdefs.h>
 #include <defs.h>
+#include <fcntl.h>
 #include <file.h>
 #include <fs.h>
 #include <param.h>
+#include <proc.h>
 #include <sleeplock.h>
 #include <spinlock.h>
 #include <stat.h>
-#include <proc.h>
-#include <fcntl.h>
-#include <fs.h>
 
 struct devsw devsw[NDEV];
 
 struct file_info gfiledescriptors[NFILE];
 int smallestFd;
 
-int fileopen(char* filepath, int mode) {
-    cprintf("MODE: %d\n", mode);
-    struct inode* newiNode = namei(filepath);
-    if(newiNode == NULL) {
-        return -1;
-    }
-    struct stat st;
-    concurrent_stati(newiNode, &st);
-    
-    struct proc* currProc = myproc();
-    if(currProc == NULL) {
-        return -1;
-    }
-
-    int type = newiNode->type;
-    if(type == T_FILE && mode != O_RDONLY) {
-        cprintf("a non-console file & non-read mode");
-        return -1;
-    }
-
-    int availableSlot = 0;
-    int processFD;
-    for(processFD = 0; processFD < NOFILE; processFD++) {
-        if(currProc->filetable[processFD] == NULL) {
-            // found an available slot
-            availableSlot = 1;
-            break;
-        }
-    }
-
-    if(availableSlot == 0) {
-        // no more valid places for a new file
-        return -1;
-    }
-
-    // place into global file descriptor
-    for(int globalFD = 0; globalFD < NFILE; globalFD++) {
-        if(gfiledescriptors[globalFD].ref == 0) {
-            gfiledescriptors[globalFD].ref++;
-            gfiledescriptors[globalFD].node = newiNode;
-            gfiledescriptors[globalFD].flags = mode;
-            // set processor to point to global file table
-            currProc->filetable[processFD] = &gfiledescriptors[globalFD];
-          return processFD;
-        }
-    }
-
+int fileopen(char *filepath, int mode) {
+  cprintf("MODE: %d\n", mode);
+  struct inode *newiNode = namei(filepath);
+  if (newiNode == NULL) {
     return -1;
+  }
+
+  locki(newiNode);
+
+  if (newiNode->type == T_DIR) {
+    unlocki(newiNode);
+    return -1;
+  }
+
+  struct stat st;
+  concurrent_stati(newiNode, &st);
+
+  struct proc *currProc = myproc();
+  if (currProc == NULL) {
+    return -1;
+  }
+
+  int type = newiNode->type;
+  if (type == T_FILE && mode != O_RDONLY) {
+    cprintf("a non-console file & non-read mode");
+    return -1;
+  }
+
+  int availableSlot = 0;
+  int processFD;
+  for (processFD = 0; processFD < NOFILE; processFD++) {
+    if (currProc->filetable[processFD] == NULL) {
+      // found an available slot
+      availableSlot = 1;
+      break;
+    }
+  }
+
+  if (availableSlot == 0) {
+    // no more valid places for a new file
+    return -1;
+  }
+
+  // place into global file descriptor
+  for (int globalFD = 0; globalFD < NFILE; globalFD++) {
+    if (gfiledescriptors[globalFD].ref == 0) {
+      gfiledescriptors[globalFD].ref++;
+      gfiledescriptors[globalFD].node = newiNode;
+      gfiledescriptors[globalFD].flags = mode;
+      // set processor to point to global file table
+      currProc->filetable[processFD] = &gfiledescriptors[globalFD];
+      unlocki(newiNode);
+      return processFD;
+    }
+  }
+
+  unlocki(newiNode);
+  return -1;
 }
 
-int filewrite(int fd, char* buffer, int writebytes) {
-    struct proc* currProc = myproc();
-    if(currProc == NULL) {
-        return -1;
-    }
+int filewrite(int fd, char *buffer, int writebytes) {
+  struct proc *currProc = myproc();
+  if (currProc == NULL) {
+    return -1;
+  }
 
-    if(currProc->filetable[fd] == NULL) {
-        return -1;
-    }
+  if (currProc->filetable[fd] == NULL) {
+    return -1;
+  }
 
-    struct file_info file = *(currProc->filetable[fd]);
-    if(file.node == NULL) {
-        cprintf("node\n");
-        return -1;
-    }
+  struct file_info file = *(currProc->filetable[fd]);
+  if (file.node == NULL) {
+    cprintf("node\n");
+    return -1;
+  }
 
-    if(file.flags == O_RDONLY) {
-        cprintf("flags\n");
-        return -1;
-    }
+  if (file.flags == O_RDONLY) {
+    cprintf("flags\n");
+    return -1;
+  }
 
-    
-    //write bytes_to_write from the buffer into the fd
-    int bytesWritten = concurrent_writei(file.node, buffer, file.currOffset, writebytes);
-    //update the current position
-    if(bytesWritten != -1){
-        file.currOffset = file.currOffset + bytesWritten;
-    }
-    cprintf("done %d\n", bytesWritten);
-    return bytesWritten;
+  // write bytes_to_write from the buffer into the fd
+  int bytesWritten =
+      concurrent_writei(file.node, buffer, file.currOffset, writebytes);
+  // update the current position
+  if (bytesWritten != -1) {
+    file.currOffset = file.currOffset + bytesWritten;
+  }
+  cprintf("done %d\n", bytesWritten);
+  return bytesWritten;
 }
 
-int fileread(int fd, char* buffer, int readbytes) {
-    struct proc* currProc = myproc();
-    if(currProc == NULL) {
-        return -1;
-    }
+int fileread(int fd, char *buffer, int readbytes) {
+  struct proc *currProc = myproc();
+  if (currProc == NULL) {
+    return -1;
+  }
 
-    if(currProc->filetable[fd] == NULL) {
-        return -1;
-    }
+  if (currProc->filetable[fd] == NULL) {
+    return -1;
+  }
 
-    struct file_info file = *(currProc->filetable[fd]);
-    if(file.node == NULL || file.flags == O_WRONLY) {
-        return -1;
-    }
+  struct file_info file = *(currProc->filetable[fd]);
+  if (file.node == NULL || file.flags == O_WRONLY) {
+    return -1;
+  }
 
-    int numRead = concurrent_readi(file.node, buffer, file.currOffset, readbytes);
-    if(numRead != -1) {
-        file.currOffset += numRead;
-    }
-    return numRead;
+  int numRead = concurrent_readi(file.node, buffer, file.currOffset, readbytes);
+  if (numRead != -1) {
+    file.currOffset += numRead;
+  }
+  return numRead;
 }
 
 int fileclose(int fd) {
-    struct proc* currProc = myproc();
-    if(currProc == NULL) {
-        return -1;
-    }
+  struct proc *currProc = myproc();
+  if (currProc == NULL) {
+    return -1;
+  }
 
-    if(currProc->filetable[fd] == NULL) {
-        return -1;
-    }
+  if (currProc->filetable[fd] == NULL) {
+    return -1;
+  }
 
-    struct file_info file = *(currProc->filetable[fd]);
-    if(file.node == NULL) {
-        return -1;
-    }
+  struct file_info file = *(currProc->filetable[fd]);
+  if (file.node == NULL) {
+    return -1;
+  }
 
-    if(file.ref == 1) {
-        // only 1 instance - remove completely
-        file.currOffset = 0;
-        file.flags = 0;
-        file.ref = 0;
-        file.node = NULL;
-    } else {
-        // multipl;e instances - remove one from the references
-        file.ref--;
-    }
+  if (file.ref == 1) {
+    irelease(file.node);
+    // only 1 instance - remove completely
+    file.currOffset = 0;
+    file.flags = 0;
+    file.ref = 0;
+    file.node = 0;
+  } else {
+    // multipl;e instances - remove one from the references
+    file.ref--;
+  }
 
-    currProc->filetable[fd] = NULL;
-    return 0;
-
+  currProc->filetable[fd] = NULL;
+  return 0;
 }
 
 int filedup(int fd) {
-    struct proc* currProc = myproc();
-    if(currProc == NULL) {
-        return -1;
-    }
-    
-    if(currProc->filetable[fd] == NULL) {
-        return -1;
-    }
-
-    struct file_info file = *(currProc->filetable[fd]);
-    if(file.node == NULL) {
-        return -1;
-    }
-    
-    int slot;
-    for(slot = 0; slot < NOFILE; slot++) {
-        if(currProc->filetable[slot] == NULL) {
-            // found the first available slot
-            currProc->filetable[slot] = currProc->filetable[fd];
-            currProc->filetable[fd]->ref++;
-            return slot;
-        }
-    }
-
+  struct proc *currProc = myproc();
+  if (currProc == NULL) {
     return -1;
+  }
+
+  if (currProc->filetable[fd] == NULL) {
+    return -1;
+  }
+
+  struct file_info file = *(currProc->filetable[fd]);
+  if (file.node == NULL) {
+    return -1;
+  }
+
+  int slot;
+  for (slot = 0; slot < NOFILE; slot++) {
+    if (currProc->filetable[slot] == NULL) {
+      // found the first available slot
+      currProc->filetable[slot] = currProc->filetable[fd];
+      currProc->filetable[fd]->ref++;
+      return slot;
+    }
+  }
+
+  return -1;
 }
 
-int filestat(int fd, struct stat* fstat) {
-    struct proc* currProc = myproc();
-    if(currProc->filetable[fd] == NULL) {
-        // invalid FD
-        return -1;
-    }
+int filestat(int fd, struct stat *fstat) {
+  struct proc *currProc = myproc();
+  if (currProc->filetable[fd] == NULL) {
+    // invalid FD
+    return -1;
+  }
 
-    struct file_info file = *(currProc->filetable[fd]);
-    if(file.node == NULL) {
-        // invalid file
-        return -1;
-    }
+  struct file_info file = *(currProc->filetable[fd]);
+  if (file.node == NULL) {
+    // invalid file
+    return -1;
+  }
 
-    // get the stats
-    concurrent_stati(file.node, fstat);
-    // return success
-    return 0;
+  // get the stats
+  concurrent_stati(file.node, fstat);
+  // return success
+  return 0;
 }
