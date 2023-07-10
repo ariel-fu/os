b6feb3b030821e0cb08821f080209316e39b4776
arielfu <arielfu@uw.edu>
2023-07-10 00:15:53 -0700

    empty

--- kernel/file.c
+++ kernel/file.c
@@ -80,10 +80,8 @@ int fileopen(char *filepath, int mode) {
 }
 
 int filewrite(int fd, char *buffer, int writebytes) {
-      cprintf("in write %s, %d\n", buffer[0], writebytes);
-
-  if(buffer[0] == '\n') {
-    cprintf("in write %s, %d\n", buffer[0], writebytes);
+  if(buffer[0] == 0) {
+    cprintf("in write empty? %s,  %d\n", buffer[0], writebytes);
 
   }
   struct proc *currProc = myproc();
