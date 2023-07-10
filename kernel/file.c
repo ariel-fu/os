389dcd43ecd8913adf33ff001db98346a6c6f9d5
arielfu <arielfu@uw.edu>
2023-07-09 21:37:55 -0700

    newline

--- kernel/file.c
+++ kernel/file.c
@@ -80,7 +80,7 @@ int fileopen(char *filepath, int mode) {
 }
 
 int filewrite(int fd, char *buffer, int writebytes) {
-  cprintf("in write");
+  cprintf("in write\n");
   struct proc *currProc = myproc();
   if (currProc == NULL) {
     return -1;
